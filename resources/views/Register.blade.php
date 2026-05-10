81c05cc4b153c4c04b7c34f96c5c341f39a5a7b1
izzamahdy26 <anggaizzamahdy@gmail.com>
2026-05-10 19:46:29 +0700

    Menyelesaikan fitur form login dan halaman dashboard

---
+++ resources/views/Register.blade.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register - Path Deck</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<body>
+    <main class="login-container">
+        <a href="index.html">
+            <div class="login-logo">Path Deck</div>
+        </a>
+        
+        <div class="login-card">
+            <div class="login-header">
+                <h2>Create Account</h2>
+                <p>Start your learning journey today.</p>
+            </div>
+            <form action="#" method="POST" class="login-form">
+ 
+                <div class="form-group">
+                    <label for="nama">NAMA</label>
+                    <input type="text" id="nama" name="nama" placeholder="Masukkan nama lengkap Anda" required>
+                </div>
+
+                <div class="form-group">
+                    <label for="email">EMAIL</label>
+                    <input type="email" id="email" name="email" placeholder="Masukkan email Anda" required>
+                </div>
+
+                <div class="form-group">
+                    <label for="password">PASSWORD</label>
+                    <input type="password" id="password" name="password" placeholder="Buat password Anda" required>
+                </div>
+
+                <div class="checkbox-group">
+                    <input type="checkbox" id="terms" name="terms" required>
+                    <label for="terms">
+                        I agree to the <a href="#">Terms of Service</a> and <a href="#">Privacy Policy</a>.
+                    </label>
+                </div>
+
+                <button type="submit" class="btn btn-primary btn-block">Register</button>
+            </form>
+
+            <div class="login-footer">
+                <p>Apakah Sudah Punya Akun? <a href="Login.blade.php">Login</a></p>
+            </div>
+        </div>
+    </main>
+
+    <!-- Bagian Footer -->
+    <footer>
+        <p>@ 2026 Path Deck</p>
+    </footer>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
-
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('LandingPage'); 
 });
+
+Route::get('/login', function () {
+    return view('Login');
+});
+
+Route::get('/register', function () {
+    return view('Register');
+});
+
+Route::post('/login-proses', [AuthController::class, 'login_proses']);
+
+Route::get('/dashboard', function () {
+    return view('Dashboard');
+})->middleware('auth');
