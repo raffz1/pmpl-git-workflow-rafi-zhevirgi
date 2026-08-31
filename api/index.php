ef3a8edf84e5edb56683855e22c7b2a54a194e95
raffz <raffz.panther@gmail.com>
2026-08-31 16:46:28 +0700

    Configure tmp storage folders for Vercel runtime

--- api/index.php
+++ api/index.php
@@ -1,2 +1,18 @@
 <?php
+
+// Buat folder storage sementara di environment serverless /tmp
+$dirs = [
+    '/tmp/storage/framework/views',
+    '/tmp/storage/framework/cache',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/logs',
+    '/tmp/bootstrap/cache'
+];
+
+foreach ($dirs as $dir) {
+    if (!is_dir($dir)) {
+        mkdir($dir, 0755, true);
+    }
+}
+
 require __DIR__ . '/../public/index.php';
